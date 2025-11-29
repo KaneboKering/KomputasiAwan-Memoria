2e69aad54b534d96e87ca6d62ab4aa05d0c92c2c
Ferdian Destrinata <d1041221025@student.untan.ac.id>
2025-11-29 13:20:37 +0700

    db

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,25 +1,17 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-// Cek apakah kita sedang di production (Cloud) atau development (Laptop)
-// Jika variabel DB_URL ada, berarti kita pakai settingan Cloud
-const dbConfig = process.env.DB_URL 
-    ? {
-        uri: process.env.DB_URL,
-        ssl: { rejectUnauthorized: false } // Penting untuk Aiven!
-      }
-    : {
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-        port: process.env.DB_PORT
-      };
+const db = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
 
-// Buat koneksi (Jika ada URI pakai uri, jika tidak pakai parameter biasa)
-const db = process.env.DB_URL 
-    ? mysql.createPool(dbConfig.uri) 
-    : mysql.createPool(dbConfig);
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
 db.getConnection((err) => {
     if (err) {
@@ -29,4 +21,4 @@ db.getConnection((err) => {
     }
 });
 
-module.exports = db.promise();
+module.exports = db.promise(); // Kita pakai mode Promise biar bisa pakai async/await nanti
